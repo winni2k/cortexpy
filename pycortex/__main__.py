6e71814c9af9ac063ecfa52b5e10757695af3d4b
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-10-02 14:17:39 +0200

    Implements pycortex print command

--- pycortex/__main__.py
+++ pycortex/__main__.py
@@ -8,19 +8,35 @@ from pycortex.cortex_graph import CortexGraphRandomAccessParser, \
 
 @attr.s(slots=True)
 class CortexGraphContigRetriever(object):
-    cgp = attr.ib()
+    graph_parser = attr.ib(default=None)
+    fh = attr.ib(default=None)
+
+    def __attrs_post_init__(self):
+        if self.graph_parser is None:
+            assert self.fh is not None
+            self.graph_parser = CortexGraphRandomAccessParser(self.fh)
 
     def get_kmers_for_contig(self, contig):
-        pass
+        kmer_size = self.graph_parser.header.kmer_size
+        assert len(contig) >= kmer_size
+        kmers = []
+        for kmer_start in range(len(contig) - kmer_size + 1):
+            kmer_string = contig[kmer_start:(kmer_start + kmer_size)]
+            kmer = self.graph_parser.get_kmer_for_string(kmer_string)
+            kmers.append((kmer, kmer_string))
+        return kmers
 
 
 def print_contig(args):
     if args.record:
         with open(args.graph, 'rb') as fh:
-            cg = CortexGraphRandomAccessParser(fh)
-            kmer = cg.get_kmer(args.record)
-
-            print(cortex_kmer_as_cortex_jdk_print_string(kmer))
+            contig_retriever = CortexGraphContigRetriever(fh=fh)
+            contig_kmers = contig_retriever.get_kmers_for_contig(args.record)
+            if len(contig_kmers) == 1:
+                print(cortex_kmer_as_cortex_jdk_print_string(contig_kmers[0][0]))
+            else:
+                for kmer, kmer_string in contig_kmers:
+                    print(cortex_kmer_as_cortex_jdk_print_string(kmer, alt_kmer_string=kmer_string))
 
 
 def main(argv):
