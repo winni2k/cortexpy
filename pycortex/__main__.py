2f7ba1b1abacc038e533387260380285729e695b
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-11-30 10:27:03 +0100

    Add version print on CLI

--- pycortex/__main__.py
+++ pycortex/__main__.py
@@ -1,11 +1,13 @@
 import argparse
 import sys
 
+import pycortex
 import pycortex.command.view as view
 
 
 def main(argv):
-    parser = argparse.ArgumentParser('pycortex')
+    parser = argparse.ArgumentParser(prog='pycortex',
+                                     description='version {}'.format(pycortex.__version__))
 
     # Copied from https://stackoverflow.com/a/26217957/528691
     subparsers = parser.add_subparsers()
