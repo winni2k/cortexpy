10b16f3fcdba1457978d5c3eb8b0bab9dbfa54ce
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-12-06 17:35:24 +0100

    Further work on branch traversal

--- cortexpy/graph/traversal/branch.py
+++ cortexpy/graph/traversal/branch.py
@@ -11,6 +11,9 @@ class Branch(object):
 
     def traverse_from(self, kmer_string, *, direction=EdgeTraversalOrientation.original):
         graph = nx.MultiDiGraph()
+        try:
+            kmer = self.ra_parser.get_kmer_for_string(kmer_string)
+            graph.add_node(kmer_string, kmer=kmer)
+        except KeyError:
+            pass
         return graph
-        # while True:
-        #     ra_parser[kmer_string]
--- cortexpy/test/test_unit/test_graph/test_traversal/test_branch.py
+++ cortexpy/test/test_unit/test_graph/test_traversal/test_branch.py
@@ -1,5 +1,4 @@
 import attr
-import pytest
 
 import cortexpy.graph.parser
 import cortexpy.test.builder as builder
@@ -9,6 +8,7 @@ import cortexpy.test.expectation as expectation
 @attr.s(slots=True)
 class BranchTestDriver(object):
     graph_builder = attr.ib(attr.Factory(builder.Graph))
+    start_kmer_string = attr.ib(None)
 
     def with_kmer(self, *args):
         self.graph_builder.with_kmer(*args)
@@ -18,16 +18,22 @@ class BranchTestDriver(object):
         self.graph_builder.with_kmer_size(n)
         return self
 
+    def with_start_kmer_string(self, start_kmer_string):
+        self.start_kmer_string = start_kmer_string
+        return self
+
     def run(self):
+        assert self.start_kmer_string is not None
         random_access_parser = cortexpy.graph.parser.RandomAccess(self.graph_builder.build())
-        graph = cortexpy.graph.traversal.Branch(random_access_parser, {}).traverse_from('AAA')
+        graph = (cortexpy.graph.traversal.Branch(random_access_parser, {})
+                 .traverse_from(self.start_kmer_string))
         return expectation.graph.KmerGraphExpectation(graph)
 
 
 class Test(object):
     def test_on_empty_graph_returns_empty_graph(self):
         # given
-        driver = BranchTestDriver().with_kmer_size(3)
+        driver = BranchTestDriver().with_kmer_size(3).with_start_kmer_string('AAA')
 
         # when
         expect = driver.run()
@@ -37,15 +43,17 @@ class Test(object):
          .has_n_nodes(0)
          .has_n_edges(0))
 
-    @pytest.mark.xfail(reason='not implemented yet')
-    def test_on_single_kmer_graph_with_one_kmer(self):
+    def test_two_unconnected_kmers_returns_graph_with_one_kmer(self):
         # given
-        driver = BranchTestDriver().with_kmer_size(3).with_kmer('AAA')
+        driver = (BranchTestDriver()
+                  .with_kmer_size(3)
+                  .with_kmer('AAA')
+                  .with_kmer('AAT')
+                  .with_start_kmer_string('AAA'))
 
         # when
         expect = driver.run()
 
         # then
-        (expect
-         .has_n_nodes(1)
+        (expect.has_nodes('AAA')
          .has_n_edges(0))
