b429243bdd0e9df881159270e59700c9380907a5
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-12-06 16:06:34 +0100

    Remove graph builder, as it is not needed

--- cortexpy/graph/__init__.py
+++ cortexpy/graph/__init__.py
@@ -1,4 +1,3 @@
 from cortexpy.graph.traversal.engine import Engine  # noqa
 from .contig_retriever import ContigRetriever  # noqa
 from .serializer import Serializer  # noqa
-from .builder import Builder  # noqa
